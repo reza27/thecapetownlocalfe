48239f5fb31e3109baf12fc77771273edf1c9bf1
rezaa27 <reza.adams@avenutech.com>
2024-03-21 00:03:52 +0200

    Update apollo-client.js

--- helpers/apollo-client.js
+++ helpers/apollo-client.js
@@ -4,6 +4,10 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 console.log('process.env.PROD_URL apollo',process.env.PROD_URL)
 const client = new ApolloClient({
   uri: process.env.APP_ENV==='production'? `${process.env.PROD_URL}/api/graphql`: `${process.env.LOCAL_URL}/api/graphql`,
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log('graphQLErrors', graphQLErrors)
+    console.log('networkError', networkError)
+  }
   //uri: `LOCAL_URL${process.env.PROD_URL}/api/graphql`,
   //uri: 'http://localhost:3000/api/graphql',
   //uri: 'https://thecapetownlocal.herokuapp.com/api/graphql',
