4c26b2ab6089a4b581af5a5b320babbc3b343d0f
rezaa27 <reza.adams@avenutech.com>
2024-04-08 00:33:22 +0200

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.scss'
 import Layout from '../components/layout'
 import '@fortawesome/fontawesome-svg-core/styles.css'; //importing font awesome css
 import { config } from '@fortawesome/fontawesome-svg-core';
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
   return (
       <Layout>
        <Component {...pageProps} />
-       <GoogleTagManager gtmId="G-NC9WGWVD6X" />
+       <GoogleAnalytics gaId="G-NC9WGWVD6X" />
       </Layout>
   )
 }
