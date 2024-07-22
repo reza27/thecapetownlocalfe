92d666b8331c4e404702f2d198aba7c269daa203
rezaa27 <reza.adams@avenutech.com>
2024-07-22 22:43:01 +0200

    Remove tsbuildinfo

--- app/about/about-page.tsx
+++ app/about/about-page.tsx
@@ -1,126 +1,140 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import $ from 'jquery';
-import { DocumentRenderer } from '@keystone-6/document-renderer';
-import Image from 'next/image';
-import ImageLoader from '../../components/image-loader';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMountain} from '@fortawesome/free-solid-svg-icons'
-import Head from 'next/head'
+"use client";
+import React, { useState, useEffect } from "react";
+import $ from "jquery";
+import { DocumentRenderer } from "@keystone-6/document-renderer";
+import Image from "next/image";
+import ImageLoader from "../../components/image-loader";
+import Head from "next/head";
 
-import { Metadata } from 'next'
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: 'About'
-}
+  title: "About",
+};
 
 const guideImageStyle = {
-    objectFit: 'cover',
-    objectPosition:'left top',
-    height: '520px',
-    width: '100%',
-    overflow: 'hidden'
+  objectFit: "cover",
+  objectPosition: "left top",
+  height: "520px",
+  width: "100%",
+  overflow: "hidden",
 };
 
 const affiliateImageStyle = {
-    objectFit: 'contain',
-    height: '120px',
-    width: '120px',
-    padding:'5px',
-    overflow: 'hidden'
+  objectFit: "contain",
+  height: "120px",
+  width: "120px",
+  padding: "5px",
+  overflow: "hidden",
 };
 
 export default function About(aData) {
+  const data = aData.data.props.data;
+  console.log("about", data);
+  const featureImageStyle = {
+    objectFit: "cover",
+    height: "500px",
+    width: "100%",
+    overflow: "hidden",
+  };
 
-    const data = aData.data.props.data;
-    console.log('about', data)
-    const featureImageStyle = {
-        objectFit: 'cover',
-        height: '500px',
-        width: '100%',
-        overflow: 'hidden'
-    };
-
-    const Affilates = () => {
-        if(data.about?.affiliations?.length > 0) {
-            return (<div className="affiliations-container">
-                <h2 className="section-heading">Affiliations</h2>
-                <div className="affiliations">
-
-                    {data.about?.affiliations.map((affiliate) => (
-                        <div className="affiliate" key={affiliate.id}>
-                            <Image
-                                loader={ImageLoader}
-                                alt={affiliate.image?.altText}
-                                src={affiliate.image?.publicUrl}
-                                sizes="(max-width: 300px) 100vw,
+  const Affilates = () => {
+    if (data.about?.affiliations?.length > 0) {
+      return (
+        <div className="affiliations-container">
+          <h2 className="section-heading">Affiliations</h2>
+          <div className="affiliations">
+            {data.about?.affiliations.map((affiliate) => (
+              <div className="affiliate" key={affiliate.id}>
+                <Image
+                  loader={ImageLoader}
+                  alt={affiliate.image?.altText}
+                  src={affiliate.image?.publicUrl}
+                  sizes="(max-width: 300px) 100vw,
                (max-width: 200px) 50vw,
                auto"
-                                width={100}
-                                height={100}
-                                style={affiliateImageStyle}
-                            />
-                        </div>
-                    ))}
-                </div>
-            </div>)
-        }
-
+                  width={100}
+                  height={100}
+                  style={affiliateImageStyle}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      );
     }
+  };
 
-    useEffect(() => {
-        const handleScroll = event => {
-            //console.log('window.scrollY', window.scrollY);
-            //if (window.scrollY > 0) {
-            $('#navbar').addClass('scrolled')
-            // }
-            // else {
-            //   $('#navbar').removeClass('scrolled')
-            // }
-        };
+  useEffect(() => {
+    const handleScroll = (event) => {
+      //console.log('window.scrollY', window.scrollY);
+      //if (window.scrollY > 0) {
+      $("#navbar").addClass("scrolled");
+      // }
+      // else {
+      //   $('#navbar').removeClass('scrolled')
+      // }
+    };
 
-        window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    },[])
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    return (
-        <>
-            <Head>
-                <title>About - The Cape Town Local</title>
-            </Head>
-            <div id="about">
-                <h2 className="main-heading">About us</h2>
-                <div className="about-description">{data.about?<DocumentRenderer document={data.about?.content?.document}/>:''}</div>
-                <div className="guides-container">
-                    <h2>Meet the team</h2>
-                    <div className="guides-description">{data.about? <DocumentRenderer document={data.about?.guidesInfo?.document}/>:''}</div>
-                    <div className="guides">
-                        {data.about ? data.about?.guides?.map((guide) => (
-                            <div className="guide" key={guide.id}>
-                                <Image
-                                    loader={ImageLoader}
-                                    alt={guide.altText}
-                                    src={guide.image?.publicUrl}
-                                    sizes="(max-width: 300px) 100vw,
+  return (
+    <>
+      <Head>
+        <title>About - The Cape Town Local</title>
+      </Head>
+      <div id="about">
+        <h2 className="main-heading">About us</h2>
+        <div className="about-description">
+          {data.about ? (
+            <DocumentRenderer document={data.about?.content?.document} />
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="guides-container">
+          <h2>Meet the team</h2>
+          <div className="guides-description">
+            {data.about ? (
+              <DocumentRenderer document={data.about?.guidesInfo?.document} />
+            ) : (
+              ""
+            )}
+          </div>
+          <div className="guides">
+            {data.about
+              ? data.about?.guides?.map((guide) => (
+                  <div className="guide" key={guide.id}>
+                    <Image
+                      loader={ImageLoader}
+                      alt={guide.altText}
+                      src={guide.image?.publicUrl}
+                      sizes="(max-width: 300px) 100vw,
                  (max-width: 200px) 50vw,
                  auto"
-                                    width={100}
-                                    height={100}
-                                    style={guideImageStyle}
-                                />
-                                <div className="guide-content">
-                                    <div className="guide-name">{guide.name}</div>
-                                    <div className="guide-title">{guide.title}</div>
-                                    <div className="guide-description">{guide.description}</div>
-                                </div>
-                            </div>
-                        )):''}
+                      width={100}
+                      height={100}
+                      style={guideImageStyle}
+                    />
+                    <div className="guide-content">
+                      <div className="guide-name">{guide.name}</div>
+                      <div className="guide-title">{guide.title}</div>
+                      <div className="guide-description">
+                        {guide.description}
+                      </div>
                     </div>
-                </div>
-                <Affilates/>
-            </div>
-        </>)
+                  </div>
+                ))
+              : ""}
+          </div>
+        </div>
+        <Affilates />
+      </div>
+    </>
+  );
 }
