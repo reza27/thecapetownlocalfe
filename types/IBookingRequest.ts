d986bd78fe822ac85a3dc43ae4a6aedab1c1c71b
rezaa27 <reza.adams@avenutech.com>
2024-07-26 12:39:30 +0200

    Update request and events

---
+++ types/IBookingRequest.ts
@@ -0,0 +1,11 @@
+export interface IBookingRequest {
+  name: string;
+  email: string;
+  subject: string;
+  date: string;
+  transportNeeded: string;
+  isDateFlexible: string;
+  address: string;
+  phone: string;
+  message: string;
+}
