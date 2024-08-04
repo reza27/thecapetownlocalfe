34a3703c85e0e6e2da51a95cebfe39c286b32a2b
rezaa27 <reza.adams@avenutech.com>
2024-08-04 13:14:03 +0200

    Fixed country code bug and updated contact schema

---
+++ app/contact/contact.schema.ts
@@ -0,0 +1,22 @@
+import { boolean, date, object, string } from "yup";
+
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
+const contactSchema = object({
+  subject: string().required("Subject is required"),
+  name: string().required("Name is required"),
+  phone: string().matches(phoneRegExp, "Mobile number is not valid"),
+  email: string().email("Invalid email address").required("Required"),
+  numberOfPeople: string().required("Number of people is required"),
+  date: date().default(() => new Date()),
+  isFlexibleDate: boolean(),
+  isTransportNeeded: boolean(),
+  address: string().when("isTransportNeeded", {
+    is: true,
+    then: (schema) => schema.required(),
+  }),
+  message: string(),
+});
+
+export default contactSchema;
