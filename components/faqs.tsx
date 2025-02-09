531df6e0fdd75c389428bfb95b90888933a81199
rezaa27 <reza.adams@avenutech.com>
2025-02-10 00:05:15 +0200

    faq wip

--- components/faqs.tsx
+++ components/faqs.tsx
@@ -12,9 +12,9 @@ export const Faqs = ({ ...props }) => {
     if (faqContainer.current) {
       setContainerW(
         faqContainer.current!.getBoundingClientRect().width -
-          106 * props.data.length
+          106 * (props.data.length - 1) -
+          40
       );
-      console.log("conttainerW", containerW);
     }
   };
 
@@ -23,7 +23,8 @@ export const Faqs = ({ ...props }) => {
     if (faqContainer.current) {
       setContainerW(
         faqContainer.current!.getBoundingClientRect().width -
-          106 * props.data.length
+          106 * (props.data.length - 1) -
+          40
       );
     }
 
@@ -35,8 +36,8 @@ export const Faqs = ({ ...props }) => {
   }, []);
 
   return (
-    <div className="flex w-full flex-col 2xl:flex-row h-[600px] 2xl:h-[500px] rounded-3xl">
-      <div className="w-full 2xl:w-1/4 text-4xl 2xl:text-5xl 3xl:text-6xl font-semibold flex flex-wrap justify-center items-center text-black">
+    <div className="flex w-full flex-col 2xl:flex-row h-[700px] 2xl:h-[500px] rounded-3xl">
+      <div className="w-full 2xl:w-1/4 text-4xl 2xl:text-6xl font-semibold flex flex-wrap justify-center items-center text-black">
         <div className="tracking-tight w-full pb-8 flex flex-col justify-center items-center 2xl:items-start 2xl:justify-start">
           <p className="text-yellow text-sm">FAQS</p>
           <div>
@@ -50,7 +51,7 @@ export const Faqs = ({ ...props }) => {
       </div>
       <div
         ref={faqContainer}
-        className="flex w-full 2xl:w-3/4 justify-end text-black tracking-tight h-[500px] 2xl:h-full"
+        className="flex w-full 2xl:w-3/4 justify-end text-black tracking-tight h-[500px] 2xl:h-full pl-10"
       >
         {props.data.map((faq, index) => (
           <div
@@ -59,12 +60,12 @@ export const Faqs = ({ ...props }) => {
             }}
             onClick={() => {
               if (currentIndex === index) {
-                setCurrentIndex(null);
+                setCurrentIndex(0);
               } else {
                 setCurrentIndex(index);
               }
             }}
-            className="flex w-28 h-full relative overflow-hidden transition-all duration-1000 cursor-pointer first:rounded-l-3xl last:rounded-r-3xl"
+            className="flex w-28 h-full relative overflow-hidden transition-all ease-out duration-500 cursor-pointer first:rounded-l-3xl last:rounded-r-3xl"
           >
             <div
               style={{
@@ -76,7 +77,7 @@ export const Faqs = ({ ...props }) => {
 
             <div className="absolute w-28 h-full overflow-hidden z-10 whitespace-nowrap">
               <div
-                className="absolute w-96 -rotate-90 origin-[0%_0%] mt-96 top-20 left-6 transition-all duration-500 font-semibold text-2xl"
+                className="absolute w-96 -rotate-90 origin-[0%_0%] mt-96 top-20 left-6 transition-all ease-out duration-500 font-semibold text-2xl"
                 style={{
                   opacity: currentIndex === index ? 0 : 1,
                   transform:
@@ -88,13 +89,19 @@ export const Faqs = ({ ...props }) => {
                 {faq.question}
               </div>
             </div>
+
             <div
               style={{
                 left: currentIndex === index ? "60px" : "108px",
               }}
-              className="absolute left-24 transition-all duration-2000 w-full"
+              className="absolute left-24 transition-all ease-out duration-500 w-full h-full"
             >
-              <div className="absolute top-10 text-6xl font-semibold w-[800px]">
+              <div
+                style={{
+                  width: containerW,
+                }}
+                className="absolute top-10 text-4xl 2xl:text-5xl font-semibold w-[800px]"
+              >
                 {faq.question}
               </div>
 
@@ -102,9 +109,11 @@ export const Faqs = ({ ...props }) => {
                 style={{
                   width: containerW,
                 }}
-                className="absolute top-32 text-lg font-medium w-[600px]"
+                className="absolute top-32 text-lg font-medium w-[600px] faq-answer overflow-scroll h-full max-h-[350px]"
               >
-                <DocumentRenderer document={faq.answer.document} />
+                <div className="h-full">
+                  <DocumentRenderer document={faq.answer.document} />
+                </div>
               </div>
             </div>
           </div>
