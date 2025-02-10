f452067b1c5898de62bf3fd390e27326c1498b66
rezaa27 <reza.adams@avenutech.com>
2025-02-10 21:55:12 +0200

    faqs desktop

--- components/faqs.tsx
+++ components/faqs.tsx
@@ -5,6 +5,7 @@ export const Faqs = ({ ...props }) => {
   const [currentIndex, setCurrentIndex] = useState<number | null>(0);
   const [hoverIndex, setHoverIndex] = useState<number | null>(0);
   const [containerW, setContainerW] = useState<number | undefined>(0);
+  const [faqOpacity, setFaqOpacity] = useState<number>(0);
 
   const faqContainer = useRef<HTMLDivElement>(null);
 
@@ -13,19 +14,22 @@ export const Faqs = ({ ...props }) => {
       setContainerW(
         faqContainer.current!.getBoundingClientRect().width -
           106 * (props.data.length - 1) -
-          40
+          60
       );
     }
   };
 
   useEffect(() => {
-    //setContainerW(faqContainer.current!.getBoundingClientRect().width);
     if (faqContainer.current) {
       setContainerW(
         faqContainer.current!.getBoundingClientRect().width -
           106 * (props.data.length - 1) -
-          40
+          60
       );
+
+      setTimeout(() => {
+        setFaqOpacity(1);
+      }, 500);
     }
 
     window.addEventListener("resize", autoResize);
@@ -36,7 +40,7 @@ export const Faqs = ({ ...props }) => {
   }, []);
 
   return (
-    <div className="flex w-full flex-col 2xl:flex-row h-[700px] 2xl:h-[500px] rounded-3xl">
+    <div className="flex w-full flex-col 2xl:flex-row h-[700px] 2xl:h-[550px] rounded-3xl">
       <div className="w-full 2xl:w-1/4 text-4xl 2xl:text-6xl font-semibold flex flex-wrap justify-center items-center text-black">
         <div className="tracking-tight w-full pb-8 flex flex-col justify-center items-center 2xl:items-start 2xl:justify-start">
           <p className="text-yellow text-sm">FAQS</p>
@@ -51,7 +55,7 @@ export const Faqs = ({ ...props }) => {
       </div>
       <div
         ref={faqContainer}
-        className="flex w-full 2xl:w-3/4 justify-end text-black tracking-tight h-[500px] 2xl:h-full pl-10"
+        className="flex w-full 2xl:w-3/4 justify-end text-black tracking-tight h-[550px] 2xl:h-full pl-10"
       >
         {props.data.map((faq, index) => (
           <div
@@ -77,7 +81,7 @@ export const Faqs = ({ ...props }) => {
 
             <div className="absolute w-28 h-full overflow-hidden z-10 whitespace-nowrap">
               <div
-                className="absolute w-96 -rotate-90 origin-[0%_0%] mt-96 top-20 left-6 transition-all ease-out duration-500 font-semibold text-2xl"
+                className="absolute w-96 -rotate-90 origin-[0%_0%] mt-[440px] top-20 left-6 transition-all min-w-[500px] ease-out duration-500 font-semibold text-2xl"
                 style={{
                   opacity: currentIndex === index ? 0 : 1,
                   transform:
@@ -89,28 +93,19 @@ export const Faqs = ({ ...props }) => {
                 {faq.question}
               </div>
             </div>
-
             <div
               style={{
                 left: currentIndex === index ? "60px" : "108px",
+                width: containerW,
+                opacity: faqOpacity,
               }}
-              className="absolute left-24 transition-all ease-out duration-500 w-full h-full"
+              className="absolute left-24 ease-out duration-500 w-full h-full opacity-1 transition-all"
             >
-              <div
-                style={{
-                  width: containerW,
-                }}
-                className="absolute top-10 text-4xl 2xl:text-5xl font-semibold w-[800px]"
-              >
+              <div className="relative pt-10 text-4xl 2xl:text-5xl font-semibold w-full">
                 {faq.question}
               </div>
 
-              <div
-                style={{
-                  width: containerW,
-                }}
-                className="absolute top-32 text-lg font-medium w-[600px] faq-answer overflow-scroll h-full max-h-[350px]"
-              >
+              <div className="relative pt-10 text-lg font-medium w-full faq-answer h-full">
                 <div className="h-full">
                   <DocumentRenderer document={faq.answer.document} />
                 </div>
